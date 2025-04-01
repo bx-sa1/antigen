273ed61376879d2ac747c745c434ca95a36f319a
Baba <bms110403@gmail.com>
2025-04-01 21:49:55 +0100

    rewrite

--- antigen/__main__.py
+++ antigen/__main__.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import csv
 from sentence import Sentence
+import pandas as pd
 
 def help():
     return '''
@@ -14,9 +15,9 @@ Usage: anti [[flag] [value]]...
 def get_sentences(req_url: str | None, to, limit):
     next_req = req_url
     count = 0
-    sentence_objs = []
+    all_sentences = pd.DataFrame(None)
 
-    while next_req is not None and len(sentence_objs) < limit:
+    while next_req is not None:
         count += 1
 
         resp = requests.get(next_req) 
@@ -25,12 +26,36 @@ def get_sentences(req_url: str | None, to, limit):
         sentences = json["data"]
         paging = json["paging"]
 
-        for s in sentences:
-            sentence_objs.append(Sentence(s, to))
+        def audios_map(row):
+            row = row.copy()
+            audios = row["audios"]
+            if len(audios) > 0:
+                audios = audios[0]["download_url"]
+            else:
+                audios = None
+            return row
 
-        next_req = str(paging["next"]) if paging["has_next"] == True else None
+        def translations_map(row):
+            row = row.copy()
+            translations = row["translations"]
+            for t in translations[0]:
+                if t["lang"] == to:
+                    translations = t["text"]
+                    return row
+            translations = None
+            return row
+            
+        sentences_table = pd.json_normalize(sentences)
+        sentences_table = sentences_table.filter(items=["text", "audios", "translations"]) \
+                                         .apply(audios_map, axis=1) \
+                                         .apply(translations_map, axis=1)
+        print(sentences_table.to_string())
 
-    return sentence_objs
+        all_sentences = pd.concat([all_sentences, sentences_table])
+
+        next_req = str(paging["next"]) if paging["has_next"] == True and all_sentences.size < limit else None
+
+    return all_sentences
 
 
     
